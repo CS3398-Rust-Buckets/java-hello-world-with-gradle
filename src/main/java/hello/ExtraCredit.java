abf6d2b57af2ab456017a393aff238c3fb567b61
Laura Adams <l_a83@txstate.edu>
2017-05-08 21:29:17 -0500

    edits 4

--- src/main/java/hello/ExtraCredit.java
+++ src/main/java/hello/ExtraCredit.java
@@ -5,6 +5,7 @@ public class ExtraCredit {
 
   private double number = 1.0;
   private double power = 1.0;
+  private int fibNum = 1;
 
   public double getNum()
   {
@@ -14,6 +15,11 @@ public class ExtraCredit {
   public double getPower()
   {
     return power;
+  }
+
+  public int getFibNum()
+  {
+    return fibNum;
   }
 
   public void setNum(double newNumber)
@@ -26,6 +32,23 @@ public class ExtraCredit {
     this.power = newPower;
   }
 
+  public void setFibNum(int newFibNum)
+  {
+    this.fibNum = newFibNum;
+  }
+
+  public double findSquare()
+  {
+    if (number == 1.0)
+    {
+      return 1.0;
+    }
+    else
+    {
+      return number*number;
+    }
+  }
+
   public double findPower()
   {
     if (number == 1.0)
@@ -35,6 +58,27 @@ public class ExtraCredit {
     else
     {
       return Math.pow(number, power);
+    }
+  }
+
+  public int findFibNum()
+  {
+    if(fibNum == 1)
+      return 0;
+    else if (fibNum == 2) 
+    {
+      return 1;
+    }
+    else
+    {
+      int [] nums = new int [fibNum];
+      nums[0] = 0;
+      nums[1] = 1;
+      for(int i = 2; i<fibNum; i++)
+      {
+        nums[i] = nums[i-1] + nums[i-2];
+      }
+      return nums[fibNum-1];
     }
   }
 }
--- src/test/java/hello/TestExtraCredit.java
+++ src/test/java/hello/TestExtraCredit.java
@@ -18,9 +18,46 @@ public class TestExtraCredit
   @Test
    public void testNumberEmpty() 
    {
-      assertEquals(e.getNum(),1.0);
-      assertEquals(e.getPower(), 1.0);
-      assertEquals(e.findPower(),1.0);
+      assertEquals(e.getNum(),1.0, 0);
+      assertEquals(e.getPower(), 1.0, 0);
+      assertEquals(e.getFibNum(), 1);
+      assertEquals(e.findSquare(),1.0, 0);
+      assertEquals(e.findPower(), 1.0, 0);
+      assertEquals(e.findFibNum(), 0);
+   }
+
+   @Test
+   public void testSquare()
+   {
+      e.setNum(3.0);
+      assertEquals(e.getNum(), 3.0, 0);
+      assertEquals(e.findSquare(), 9.0, 0);
+   }
+
+   @Test
+   public void testPower()
+   {
+      e.setNum(2.0);
+      assertEquals(e.getNum(), 2.0, 0);
+      e.setPower(3.0);
+      assertEquals(e.getPower(), 3.0, 0);
+      assertEquals(e.findPower(), 8.0, 0);
+   }
+
+   @Test
+   public void testFibNum1()
+   {
+      e.setFibNum(2);
+      assertEquals(e.getFibNum(), 2);
+      assertEquals(e.findFibNum(), 1);
+   }
+
+   @Test
+   public void testFibNum2()
+   {
+      e.setFibNum(5);
+      assertEquals(e.getFibNum(), 5);
+      assertEquals(e.findFibNum(), 3);
    }
 
 }
