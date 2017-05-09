39d3bbf750963dc9859371b739f227e09a2285a3
Laura Adams <l_a83@txstate.edu>
2017-05-08 19:38:24 -0500

    edits

---
+++ src/main/java/hello/Extra_Credit_Laura.java
@@ -0,0 +1,63 @@
+
+/*****************************************************
+hello package template:
+
+package hello;
+
+public class Greeter {
+
+  private String name = "";
+
+  public String getName() 
+  {
+    return name;
+  }
+
+  public void setName(String name) 
+  {
+      this.name = name;
+  }
+   
+  public String sayHello() 
+  {
+    if (name == "") 
+    {
+       return "Hello!";
+    }
+    else 
+    {
+       return "Hello " + name + "!";
+    }
+  } 
+}
+***************************************************/
+
+package extraCredit;
+
+public class mathWorks {
+
+  private int number = 1;
+
+  public int getNum()
+  {
+    return number;
+  }
+
+  public void setNum(int newNumber)
+  {
+    this.number = newNumber;
+  }
+
+  public int getCube()
+  {
+    if (number == 1)
+    {
+      return 1;
+    }
+    else
+    {
+      
+    }
+  }
+}
+
